9e129a4e71ca0a683a738a984c14b72358087154
empty-works <pama.mark@gmail.com>
2020-06-07 21:55:18 +0200

    Initial readline interface.

---
+++ bmi-calc.js
@@ -0,0 +1,14 @@
+/* If my weight is 65Kg and my height is 1.8m, I should be able to call your function like this:
+
+var bmi = bmiCalculator(65, 1.8); 
+
+bmi should equal 20 when it's rounded to the nearest whole number.
+
+*/
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
